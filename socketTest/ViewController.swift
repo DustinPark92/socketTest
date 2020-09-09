591f52bce5c65e1543eb5b53d03889c692260b62
DustinPark92 <p4569zz@gmail.com>
2020-09-09 14:06:38 +0900

    Initial Commit

---
+++ socketTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  socketTest
+//
+//  Created by Dustin on 2020/09/09.
+//  Copyright © 2020 Dustin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
